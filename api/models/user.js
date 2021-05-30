7b0b81f1151f0d1e2f4f40cdf197aa59a12c778e
Walter Verpoest <walter.verpoest@gmail.com>
2021-05-30 12:54:19 +0200

    User model updated

--- api/models/user.js
+++ api/models/user.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: { 
-        type: String, 
-        required: true, 
-        unique: true, 
+    email: {
+        type: String,
+        required: true,
+        unique: true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+        required: true,
+    },
 });
 
 module.exports = mongoose.model('User', userSchema);
